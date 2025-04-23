4a17b34322d8cbfeaec1fa8d2204d26d9af1d2c0
Noeh <noehboiser221@gmail.com>
2025-04-23 17:17:50 +0800

    good to go

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.qqq"
+        applicationId = "com.example.OperationBoom"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
